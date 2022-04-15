42a00d287e5142f487dd197df15cd8704ed75405
Depyume <101354941+Depyume@users.noreply.github.com>
2022-04-15 18:59:55 +0200

    MapManager

--- src/game.py
+++ src/game.py
@@ -3,7 +3,7 @@ import pytmx
 import pyscroll
 
 from src.player import Player
-from src.map import Map
+from src.map import MapManager
 
 
 class Game:
@@ -13,31 +13,18 @@ class Game:
         # fenetre d jeu
         self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Depgame - Aventure")
+        self.player = Player(0, 0)
 
         # charger carte
-        tmx_data = pytmx.util_pygame.load_pygame('world.tmx')
-        map_data = pyscroll.data.TiledMapData(tmx_data)
-        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-        map_layer.zoom = 3
+        # tmx_data = pytmx.util_pygame.load_pygame('../maps/world.tmx')
+        # map_data = pyscroll.data.TiledMapData(tmx_data)
+        # map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        # map_layer.zoom = 3
+        self.world = MapManager(self.screen, self.player)
 
         # gene un player
-        player_position = tmx_data.get_object_by_name("Player")
-        self.player = Player(player_position.x, player_position.y)
-
-        # def un lister pour stocker le hitbox et les collision
-        self.walls = []
-
-        for obj in tmx_data.objects:
-            if obj.type == "collision":
-                self.walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
-
-        # dessiner le groupe de claque
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=2)
-        self.group.add(self.player)
-
-        # definir le rect de coli pour entrer dans la maison
-        enter_house = tmx_data.get_object_by_name('enter_house')
-        self.enter_house_rect = pygame.Rect(enter_house.x, enter_house.y, enter_house.width, enter_house.height)
+        # player_position = tmx_data.get_object_by_name("Player")
+        # self.player = Player(player_position.x, player_position.y)
 
     def handle_input(self):
         pressed = pygame.key.get_pressed()
@@ -51,14 +38,11 @@ class Game:
             self.player.move_right()
 
     def update(self):
-        self.group.update()
+        self.world.update()
 
-        # verif de la collision
-        if self.player.feet.collidelist(self.walls) > -1:
-            self.player.move_back()
-        #for sprite in self.group.sprites():
-            #if sprite.feet.collidelist(self.walls) > -1:
-               # sprite.move_back()
+        # for sprite in self.group.sprites():
+        # if sprite.feet.collidelist(self.walls) > -1:
+        # sprite.move_back()
 
     def run(self):
 
@@ -71,8 +55,7 @@ class Game:
             self.player.save_location()
             self.handle_input()
             self.update()
-            self.group.center(self.player.rect)
-            self.group.draw(self.screen)
+            self.world.draw()
             pygame.display.flip()
 
             for event in pygame.event.get():
--- src/map.py
+++ src/map.py
@@ -16,71 +16,72 @@ class Warp:
 @dataclass
 class Map:
     name: str
-    walls: list[pygame.Rect]
-    warps: list[Warp]
+    walls: list
     group: pyscroll.PyscrollGroup
+    warps: list
 
-    class MapManager:
 
-        def __init__(self, screen, player):
-            self.maps = dict()
-            self.screen = screen
-            self.player = player
-            self.current_map = "world"
+class MapManager:
 
-            self.register_map("world")
-            self.register_map("house 1")
+    def __init__(self, screen, player):
+        self.maps = dict()
+        self.screen = screen
+        self.player = player
+        self.current_map = "world"
 
-        def check_collision(self):
-            # warps
-            #for warps in self.get_map().warps:
-             #   if warps.from_world == self.current_map:
-              #      point = self.get_object(warps.origin_point)
-               #     rect = pygame.Rect(point.x, point.y, point.width, point.height)
+        self.register_map("world")
+        self.register_map("house 1")
 
-#                    if self.player.feet.colliderect(rect):
- #                       copy_warps = warps
-  #                      self.current_map = warps.target_world
-   #                     self.teleport_player(copy_warps.warps_point)
-            # collision
-            for sprite in self.get_group().sprites():
-                if sprite.feet.collidelist(self.get_walls()) > -1:
-                    sprite.move_back()
+    def check_collision(self):
+        # warps
+        # for warps in self.get_map().warps:
+        #   if warps.from_world == self.current_map:
+        #      point = self.get_object(warps.origin_point)
+        #     rect = pygame.Rect(point.x, point.y, point.width, point.height)
 
-        def teleport_player(self, name):
-            point = self.get_object(name)
-            self.player.position[0] = point.x
-            self.player.position[1] = point.y
-            self.player.save_location()
+        #                    if self.player.feet.colliderect(rect):
+        #                       copy_warps = warps
+        #                      self.current_map = warps.target_world
+        #                     self.teleport_player(copy_warps.warps_point)
+        # collision
+        for sprite in self.get_group().sprites():
+            if sprite.feet.collidelist(self.get_walls()) > -1:
+                sprite.move_back()
 
-        def register_map(self, name):
-            # charger map
-            tmx_data = pytmx.util_pygame.load_pygame(f"../{name}.tmx")
-            map_data = pyscroll.data.TiledMapData(tmx_data)
-            map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-            map_layer.zoom = 3
+    def teleport_player(self, name):
+        point = self.get_object(name)
+        self.player.position[0] = point.x
+        self.player.position[1] = point.y
+        self.player.save_location()
 
-            # lister les zones de collisions de la map
-            walls = []
-            warps = []
-            for obj in tmx_data.objects:
-                if obj.type == "collision":
-                    walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
-                    continue
-                if obj.type == "warp":
-                    warps.append(Warp(
-                        obj.name,
-                        obj.properties["warp_to"],
-                        obj.properties["warp_point"],
-                        pygame.Rect(obj.x, obj.y, obj.width, obj.height)
-                    ))
+    def register_map(self, name):
+        # charger map
+        tmx_data = pytmx.util_pygame.load_pygame(f"../maps/{name}.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 3
 
-            # dessiner le groupe de calqque
-            group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=2)
-            group.add(self.player)
+        # lister les zones de collisions de la map
+        walls = []
+        warps = []
+        for obj in tmx_data.objects:
+            if obj.type == "collision":
+                walls.append(pygame.Rect(obj.x, obj.y, obj.width, obj.height))
+                continue
+            if obj.type == "warp":
+                warps.append(Warp(
+                    obj.name,
+                    obj.properties.get("warp_to"),
+                    obj.properties.get("warp_point"),
+                    pygame.Rect(obj.x, obj.y, obj.width, obj.height)
+                ))
 
-            # creer un obj map
-            self.maps[name] = Map(name, walls, group, tmx_data, warps)
+        # dessiner le groupe de calqque
+        group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=2)
+        group.add(self.player)
+
+        # creer un obj map
+        self.maps[name] = Map(name, walls, group, warps)
 
     def get_map(self):
         return self.maps[self.current_map]
