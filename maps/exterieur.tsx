8706434c477375d9146164098aeb6b52ed47f766
Depyume <101354941+Depyume@users.noreply.github.com>
2022-04-16 21:27:19 +0200

    add set

---
+++ maps/exterieur.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="exterieur" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
+ <image source="exterieur.png" trans="000000" width="1024" height="1024"/>
+</tileset>
